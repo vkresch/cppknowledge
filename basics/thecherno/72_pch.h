a69cf0995200141d616c331e9229afbdba8cad38
Viktor Kreschenski <viktor.kreschenski@altran.com>
2020-10-21 15:58:13 +0200

    Precompiled headers

---
+++ basics/thecherno/72_pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Basic C++ libs
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <utility>
+
+// Data structures
+#include <string>
+#include <stack>
+#include <array>
+#include <vector>
+#include <map>
---
+++ basics/thecherno/72_precompiled_headers.cpp
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=eSI4wctZUto&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=72
+// Precompile headers *.pch
+// Do not put frequently changing files into the precompiled headers
+// Adds convenience
+#include "72_pch.h" // This needs to be recompiled every single time
+
+// Compile with "g++ -std=c++11 72_pch.h"
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
